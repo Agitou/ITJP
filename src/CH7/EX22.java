ea3fc01439dd04fcc75a17f8d1cb79237fd40fc6
Agitou <agitouunuu@gmail.com>
2017-01-22 03:22:46 +0800

    With new exercises finished(up to 7.22): -22 with row and column check done.

--- src/CH7/EX22.java
+++ src/CH7/EX22.java
@@ -9,26 +9,66 @@ public class EX22 {
 
     public static void main(String[] args){
 
-        boolean[] board = new int[64];
-        int pos = random(0, board.length - 1);
+        boolean[] board = new boolean[64];
 
+        int qcount = 0;
+
+        while(qcount < 8){
+            int pos = random(0, board.length - 1);
+
+            if(checkPosition(board, pos)) {
+                board[pos] = true;
+                qcount++;
+            }
+        }
+
+        for(int i = 0; i < board.length; i++){
+            System.out.print("|");
+            if(board[i]) {
+                System.out.print("Q");
+            }
+            else
+                System.out.print(" ");
+
+            if((i + 1) % 8 == 0){
+                System.out.print("|\n");
+            }
+
+        }
+
+    }
+
+    public static boolean checkPosition(boolean[] board, int pos){
+
+        return possibleColumn(board, pos) && possibleRow(board, pos)
+                && minDiagonal(board, pos); //&& majDiagonal(board, pos);
     }
 
     public static boolean possibleRow(boolean[] board, int pos){
 
-        boolean isPossible = true;
-        int rowNumber = 0;
+        int rowInitValue = 0;
+        int rowEndValue = 0;
+        int posCopy = pos;
 
-        //find the belonging row and loop through it to check row availability
-        for(int i = 0, j = 7; j < board.length; i += 8, j+= 8) {
-            if(pos >= i && pos <= j) {
-                for (int k = rowNumber * 8; k < k + 8; k++) {
-                    if (board[k] && k != pos)
-                        return false;
-                }
-                break;
-            }
-            rowNumber++;
+        while(posCopy % 8 != 0){
+            rowInitValue = posCopy;
+            posCopy--;
+        }
+
+        posCopy = pos;
+
+        while(posCopy % 8 != 0){
+            rowEndValue = posCopy;
+            posCopy++;
+        }
+
+        if(rowInitValue != 0)
+            rowInitValue--;
+
+
+        for(int i = rowInitValue ; i < rowEndValue + 1; i++){
+            if(board[i])
+                return false;
         }
 
         return true;
@@ -41,22 +81,20 @@ public class EX22 {
         int colEndValue = 0;
         int posCopy = pos;
 
-        //find first and last index of the column
-        while(posCopy >= 0){
-            colInitValue = pos;
+        while(posCopy >= 0 || (posCopy + 1) % 8 != 0){
+            colInitValue = posCopy;
             posCopy -= 8;
         }
 
         posCopy = pos;
 
-        while(pos < board.length){
-            colEndValue = pos;
+        while(posCopy < board.length || posCopy % 8 != 0){
+            colEndValue = posCopy;
             posCopy += 8;
         }
 
-        //loop through the column values to see if its filled
-        for(int i = colInitValue; i < colEndValue; i += 8){
-            if(board[i] && i != pos)
+        for(int i = colInitValue; i <= colEndValue; i += 8){
+            if(board[i])
                 return false;
         }
 
@@ -64,7 +102,7 @@ public class EX22 {
 
     }
 
-    public static boolean minDiagonal(int[] board, int pos){
+    public static boolean minDiagonal(boolean[] board, int pos){
 
         int DiaInitValue = 0;
         int DiaEndValue = 0;
@@ -77,14 +115,41 @@ public class EX22 {
 
         posCopy = pos;
 
-        while(posCopy < board.length){
-            DiaEndValue = posCopy;
-            posCopy += 7;
+
+        for(int i = DiaInitValue; i <= DiaEndValue; i += 7){
+            if(board[i])
+                return false;
         }
+
+        return true;
 
     }
 
-    public static boolean majDiagonal(int[] board, int pos)
+    public static boolean majDiagonal(boolean[] board, int pos){
+
+        int DiaInitValue = 0;
+        int DiaEndValue = 0;
+        int posCopy = pos;
+
+        while(posCopy >= 0){
+            DiaInitValue = posCopy;
+            posCopy -= 9;
+        }
+
+        posCopy = pos;
+
+        while(posCopy < board.length){
+            DiaEndValue = posCopy;
+            posCopy += 9;
+        }
+
+        for(int i = DiaInitValue; i < DiaEndValue; i += 8){
+            if(board[i] && i != pos)
+                return false;
+        }
+
+        return true;
+    }
 
     public static int random(int min, int max){
 
